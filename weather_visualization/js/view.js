a27dab337e7459598e1dc5bdf71d41cefdeeb4ec
nearlynight <t.fastnacht@googlemail.com>
2015-11-02 18:05:24 +0100

    worked on style

--- weather_visualization/js/view.js
+++ weather_visualization/js/view.js
@@ -8,13 +8,29 @@ var STARTANGLE = 45;
 var ENDANGLE = 180;
 var AMOUNTBARS = 5;
 var ANGLEBETWEENBARS = 5;
-var DAYBARWIDTH = 600;
+var DAYBARWIDTH = 730;
 //var ACTIVEDAY = 1;
 var POLYGONS = [];
 var SLIDERS = [];
 var MOUSEOVERPOLYGONS = [];
 var MOUSECLICKPOLYGONS = [];
 var DRAWGRAPHARRAY = [];
+
+// COLORS
+var	C1 = "#399DCC";
+var C2 = "#6A8A99";
+var C3 = "#60FFDC";
+var C4 = "#FF897B";
+var C5 = "#CC6C78";
+var C6 = "#FCDCC0";
+var C7 = "#833D1B";
+var C8 = "#1B3547";
+var C9 = "#E84E43";
+var C10 = "#317EAD";
+var C11 = "#57B644";
+var C12 = "#FF0D00";
+
+
 var RANGE = {
 	temp: {
 		min: -7,
@@ -178,20 +194,23 @@ function initControls(callBack) {
 		};
 		//console.log("TEST+");
 
-
+		// ADD YEARS TO POPUP
 		for(var i = 0; i < YEARS.length; i++) {
 			if (!YEARS[i].active) {
 				var listEl = new ListEl(YEARS[i]);
 			}
 		}
 
+		// CLOSE ADD YEAR POPUP
 		this.button = document.createElement("button");
+		this.button.className ="closeYearButton";
 		this.button.innerHTML = "close";
 		$(this.button).on("click", {obj:this}, function(event) {
 			$("#popup").fadeOut();
 		});
+
 		$("#popup").append(this.button);
-		
+
 		//var year = document.getElementById("year_input_1").value;
 		//console.log("YEAR: " + year);
 		var popupPosition = window.innerWidth/2 - 100;
@@ -226,14 +245,31 @@ function Slider(year){
 	this.min = 0;
 	this.max = CountElementsOfYear(this.year);
 	this.averagevalues = [];
-	this.color = "red";
+	this.color = C12;
 	if (this.year == 2004) {
-		this.color = "green";
+		this.color = C1;
 	} else if (this.year == 2005) {
-		this.color = "blue";
+		this.color = C2;
 	} else if (this.year == 2006) {
-		this.color = "yellow";
+		this.color = C3;
+	} else if (this.year == 2007) {
+		this.color = C4;
+	} else if (this.year == 2008) {
+		this.color = C5;
+	} else if (this.year == 2009) {
+		this.color = C6;
+	} else if (this.year == 2010) {
+		this.color = C7;
+	} else if (this.year == 2011) {
+		this.color = C8;
+	} else if (this.year == 2012) {
+		this.color = C9;
+	} else if (this.year == 2013) {
+		this.color = C10;
+	} else if (this.year == 2014) {
+		this.color = C11;
 	}
+	
 	
 	for (var i = 0; i < AVERAGEVALUES.length; i++) {
 		if (this.year == AVERAGEVALUES[i].datetime.getFullYear()) {
@@ -261,6 +297,8 @@ function Slider(year){
 								'<span class="highlight"> </span><div id="dateview' + this.year + '" class="dateview"></div>' + 
 								'<span class="yearName">' + this.year + '</span> <button class="closeButton" id="closeButton' + this.year + '"> X </button></form>';
 		$("#sliders").append(this.div);
+
+		// CLOSE YEAR BUTTON
 		var closeButton = document.getElementById("closeButton" + this.year);
 		$(closeButton).on('click', {obj:this}, function (event) {
 			var that = event.data.obj;
@@ -269,10 +307,41 @@ function Slider(year){
 			updateBars();
 		});
 
+
+
+
 		$('#rangeInput' + this.year).on('input', {obj:this}, function (event) {
 			var that = event.data.obj;
 			var rangeEl = document.getElementById("rangeInput" + that.year);
 			var dateviewEl = document.getElementById("dateview" + that.year);
+
+			var color = C12;
+			if (that.year == 2004) {
+				color = C1;
+			} else if (that.year == 2005) {
+				color = C2;
+			} else if (that.year == 2006) {
+				color = C3;
+			} else if (that.year == 2007) {
+				color = C4;
+			} else if (that.year == 2008) {
+				color = C5;
+			} else if (that.year == 2009) {
+				color = C6;
+			} else if (that.year == 2010) {
+				color = C7;
+			} else if (that.year == 2011) {
+				color = C8;
+			} else if (that.year == 2012) {
+				color = C9;
+			} else if (that.year == 2013) {
+				color = C10;
+			} else if (that.year == 2014) {
+				color = C11;
+			}
+
+
+			$("slider-thumb").css({"border-top": "solid 20px red"});
 
 			// get active day
 			var value = parseInt($(rangeEl).val(), 10);
@@ -280,11 +349,13 @@ function Slider(year){
 			
 			// dateview position calculation
 			$(dateviewEl).html(that.averagevalues[value].datetime.toDayMonth());
-			var dateLeftPosition = (DAYBARWIDTH / (that.averagevalues.length-1)) * value;
+			var dateLeftPosition = (DAYBARWIDTH / (that.averagevalues.length)) * value;
 			$(dateviewEl).css({
-				"left" : (CANVAS.offsetLeft + dateLeftPosition) + "px",
-				"top" : (rangeEl.offsetTop + rangeEl.offsetHeight*2 + 0) + "px",
-				"display" : "block"
+				"left" : (CANVAS.offsetLeft + dateLeftPosition - 20) + "px",
+				"top" : (rangeEl.offsetTop + rangeEl.offsetHeight*2 - 35) + "px",
+				"display" : "block",
+				"height" : "5px",
+				"background-color": color
 			});
 			updateBars();
 			drawGraphs();
@@ -387,8 +458,11 @@ function drawGraphs(key, year) {
 		});
 	}
 
-	var graph_width = 380;
-	var graph_height = 750;
+	var graph_width = 750;
+	var graph_height = 380;
+	var graph_plot_compression = (graph_height/100) * 70;
+	var graph_plot_position_y = graph_height - ((graph_height/100)*10);
+
 	GRAPH_CANVAS = document.getElementById("graphCanvas");
 	GRAPH_CONTEXT = GRAPH_CANVAS.getContext("2d");
 	GRAPH_CONTEXT.clearRect(0, 0, GRAPH_CANVAS.width, GRAPH_CANVAS.height);
@@ -396,9 +470,31 @@ function drawGraphs(key, year) {
 	// DRAW COORDINATE SYSTEM
 	GRAPH_CONTEXT.beginPath();
 	GRAPH_CONTEXT.moveTo(20,20);
-	GRAPH_CONTEXT.lineTo(20,graph_width);
-	GRAPH_CONTEXT.lineTo(graph_height,graph_width);
+	GRAPH_CONTEXT.lineTo(20,graph_height);
+	GRAPH_CONTEXT.lineTo(graph_width,graph_height);
+	GRAPH_CONTEXT.lineWidth = 2;
+	GRAPH_CONTEXT.strokeStyle = "black";
 	GRAPH_CONTEXT.stroke();
+	// draw month sections
+	
+	GRAPH_CONTEXT.translate((graph_width/12)-(graph_width/24), graph_height + 10);
+	GRAPH_CONTEXT.font = "10px Roboto";
+	GRAPH_CONTEXT.fillStyle = "black";
+	var step = graph_width/12;
+	GRAPH_CONTEXT.fillText("Jan", step * 0, 0);
+	GRAPH_CONTEXT.fillText("Feb", step * 1, 0);
+	GRAPH_CONTEXT.fillText("Mar", step * 2, 0);
+	GRAPH_CONTEXT.fillText("Apr", step * 3, 0);
+	GRAPH_CONTEXT.fillText("May", step * 4, 0);
+	GRAPH_CONTEXT.fillText("Jun", step * 5, 0);
+	GRAPH_CONTEXT.fillText("Jul", step * 6, 0);
+	GRAPH_CONTEXT.fillText("Aug", step * 7, 0);
+	GRAPH_CONTEXT.fillText("Sep", step * 8, 0);
+	GRAPH_CONTEXT.fillText("Oct", step * 9, 0);
+	GRAPH_CONTEXT.fillText("Nov", step * 10, 0);
+	GRAPH_CONTEXT.fillText("Dec", step * 11, 0);
+	GRAPH_CONTEXT.translate(-((graph_width/12)-(graph_width/24)), -(graph_height + 10));
+
 	//console.log("GRAPH: " + AVERAGEVALUES.length);
 	for (var j = 0; j < DRAWGRAPHARRAY.length; j++) {
 		var year = DRAWGRAPHARRAY[j].year;
@@ -410,34 +506,66 @@ function drawGraphs(key, year) {
 
 		var color = null;
 		if (year == 2004) {
-			color = "green";
+			color = C1;
 		} else if (year == 2005) {
-			color = "blue";
+			color = C2;
 		} else if (year == 2006) {
-			color = "yellow";
+			color = C3;
 		} else if (year == 2007) {
-			color = "red";
+			color = C4;
+		} else if (year == 2008) {
+			color = C5;
+		} else if (year == 2009) {
+			color = C6;
+		} else if (year == 2010) {
+			color = C7;
+		} else if (year == 2011) {
+			color = C8;
+		} else if (year == 2012) {
+			color = C9;
+		} else if (year == 2013) {
+			color = C10;
+		} else if (year == 2014) {
+			color = C11;
 		} else {
-			color = "black";
+			color = C12;
 		}
 
-		var radius = 2;
+		var radius = null;
 	
-
+		var valueinpercent_temp = null;
 
 		for(var i=0; i <= values.length; i++) {
 			var pointX = 20 + i*2;
+			var pointX_2 = 20 + (i+1)*2;
 			if (typeof values[i] != "undefined") {
 				if (values[i].datetime == activeSlider.activeday.datetime){
-					radius = 10;
+					radius = 7;
 				} else {
 					radius = 2;
 				}
 				//console.log("HERE: " + AVERAGEVALUES[i][key]);
 				var valueinpercent = (values[i][key] - getMinMax(key).min) / (getMinMax(key).max - getMinMax(key).min);
-				var pointY = valueinpercent * 300;
+
+				//console.log(values[i+1][key]);
+				//var valueinpercent_2 = (values[i][key] - getMinMax(key).min) / (getMinMax(key).max - getMinMax(key).min);
+				var pointY = valueinpercent * graph_plot_compression;
+				//var pointY_2 = valueinpercent_2 * 300;
 				GRAPH_CONTEXT.fillStyle = "#31CDD5";
-				drawCircle(pointX, 360-pointY, radius, color, GRAPH_CONTEXT);
+
+				// DRAW LINE
+
+				GRAPH_CONTEXT.beginPath();
+				GRAPH_CONTEXT.moveTo(pointX,graph_plot_position_y - valueinpercent_temp);
+				GRAPH_CONTEXT.lineTo(pointX_2,graph_plot_position_y - pointY);
+				//GRAPH_CONTEXT.lineTo(graph_height,graph_width);
+				GRAPH_CONTEXT.lineWidth = .2;
+				GRAPH_CONTEXT.strokeStyle = "black";
+				GRAPH_CONTEXT.stroke();
+
+				valueinpercent_temp = valueinpercent * graph_plot_compression;
+
+				drawCircle(pointX, graph_plot_position_y - pointY, radius, color, GRAPH_CONTEXT);
 				//GRAPH_CONTEXT.fillRect(pointX, 360 - pointY, 2, 2); // fill in the pixel at (10,10)
 			}
 			//console.log("HERE: " + key);
@@ -479,7 +607,7 @@ function updateBars() {
 
 	//CONTEXT.fillStyle = "#00AAAA";
 
-	console.log(SLIDERS);
+	//console.log(SLIDERS);
 
 	for (var i = 0; i < SLIDERS.length; i++){
 		var tempBool = false;
